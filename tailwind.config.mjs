5674ad04b85e81fe8db6e8e68a981734fba4776d
Yordani <yalvarezb93@gmail.com>
2024-03-17 15:36:15 +0300

    all files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
